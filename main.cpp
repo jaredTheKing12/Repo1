35945137abbb332747ab1224e830b4ec3ab898c0
Your Name <you@example.com>
2023-09-07 17:20:50 -0400

    Datatype

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
+
+using namespace std; // if you don't want to type std::
+
 int main()
 {
-  std::cout << "Hello Jared" << std::endl;
-  std::cout << "Hello Jared" << std::endl;
+  // Datatypes
+  // Primitive Datatypes (Basic ones)
+  float f = 0.1;
+  int x = 1; // integer (whole number)
+  double y = 2.2; // 64-bit floating-point (real number)
+  float yy = 3.1; // 32-bit floating-point (real number)
+  char z = 'a'; // character, don't forget a single quote ''
+  std::string s = "abc"; // multiple characters ""
+  bool a = 0; // either 0 or 1
+  unsigned int xx = 10; // non-negative integer
+  unsigned char zz = '2';
+  int8_t ii = 12; // 8-bit integer
+  auto some_name = 13.0;
+  auto dd = 'e';
+
+  // Fancy Ones
 }
 // I don't llke to code
